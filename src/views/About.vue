1d59043b2d4c1d80475c27b7f34a045aa4d34e04
Eduardo Almeida <eduardo.oliveira@facens.br>
2021-11-24 21:48:46 -0300

    feat(pages): added a different layout for each page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="about" class="pa-4">
     <v-container fluid class="pt-0 pb-0 fill-height">
-      Opa
+      <h1>Feito por Eduardo Almeida, RA: 190963</h1><br/><br/>
+
+      <a
+        href="https://www.linkedin.com/in/eduardo-almeida-0a09241a8/"
+        class="text-capitalize px-1"
+        target="_blank"
+      >LINKEDIN</a>
     </v-container>
   </div>
 </template>
@@ -21,6 +27,5 @@ export default {
 <style lang="scss">
   #about {
     color: #333;
-    font-size: 5rem;
   }
 </style>
--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="pokemon" class="pa-4">
     <v-container fluid class="pt-0 pb-0 fill-height">
-      Opa
+      <h1>Caso queira jogar um jogo maneiro sobre pokémon e LIG4, clone esse repositório e se divirta</h1><br/><br/>
+      
+      <a
+        href="https://github.com/DuduAlmeida/pavi.web.vanilla.pokelig4"
+        class="text-capitalize px-1"
+        target="_blank"
+      >ACESSAR REPOSITÓRIO</a>
     </v-container>
   </div>
 </template>
